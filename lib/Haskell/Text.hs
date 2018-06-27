c73c1661bd23e35cd4032d96521277907625dd9f
Andrew Cowie <andrew@operationaldynamics.com>
2018-06-27 22:11:55 +1000

    Add library and experiment stubs

---
+++ lib/Haskell/Text.hs
@@ -0,0 +1,6 @@
+module Haskell.Text where
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+
---
+++ tests/Experiment.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Text (Text)
+import qualified Data.Text.IO as T
+
+main :: IO ()
+main = do
+    putStrLn "Done"
