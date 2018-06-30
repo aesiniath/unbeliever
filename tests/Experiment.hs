822b7630902247e7f9e742c68f215baaabb94112
Andrew Cowie <andrew@operationaldynamics.com>
2018-06-30 22:25:01 +1000

    Tweak experiment to exercise Program type

--- tests/Experiment.hs
+++ tests/Experiment.hs
@@ -3,13 +3,15 @@
 {-# OPTIONS_GHC -fno-warn-unused-top-binds #-}
 {-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 
-import Data.Text (Text)
+--import Data.Text (Text)
 import qualified Data.Text.IO as T
 import qualified Data.ByteString.Char8 as S
 import qualified Data.HashMap.Strict as HashMap
 
 import Core.Text
 import Core.Json
+import Core.Program
+import Core.System
 
 k = JsonKey "name"
 v = JsonString "Hello"
@@ -17,11 +19,34 @@ j = JsonObject (HashMap.fromList [(k, v)])
 
 b = StrictBytes (S.pack "{\"cost\": 4500}")
 
+program :: Program ()
+program = do
+    name <- getProgramName
+    write stdout name
+    setProgramName "hello"
+    name <- getProgramName
+    write stdout name
+    liftIO $ do
+        let x = encodeToUTF8 j
+        print x
+
+        let (Just y) = decodeFromUTF8 b
+        print y
+        print (encodeToUTF8 y)
+
+write :: Handle -> Text -> Program ()
+write h t = liftIO $ do
+    T.hPutStrLn h (fromText t)
+
 main :: IO ()
 main = do
-    let x = encodeToUTF8 j
-    print x
+{-
+    context <- configure $ do
+        -- blah
+        return ()
 
-    let (Just y) = decodeFromUTF8 b
-    print y
-    print (encodeToUTF8 y)
+    execute' context program
+-}
+    execute program
+
+
