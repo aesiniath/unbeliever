c29113394dcb101d8ab9cadb935b8227aec6dc95
Andrew Cowie <andrew@operationaldynamics.com>
2018-07-18 21:43:57 +1000

    Use explicit intermediate functions between JSON types

--- lib/Core/Json.hs
+++ lib/Core/Json.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE StrictData #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Core.Json
     ( encodeToUTF8
     , decodeFromUTF8
+    , JsonValue(..)
+    , JsonKey(..)
     ) where
 
 import qualified Core.Text as Core
@@ -17,13 +20,20 @@ import Data.Hashable (Hashable)
 import Data.Scientific (Scientific)
 import qualified Data.Scientific as Scientific
 import Data.String (IsString)
+import qualified Data.Text as T (Text)
+import qualified Data.Vector as V
 import GHC.Generics
 
 encodeToUTF8 :: JsonValue -> Core.Bytes
-encodeToUTF8 = Core.StrictBytes . S.concat . L.toChunks . Aeson.encode
+encodeToUTF8 = Core.StrictBytes . S.concat . L.toChunks . Aeson.encode . intoAeson
 
 decodeFromUTF8 :: Core.Bytes -> Maybe JsonValue
-decodeFromUTF8 (Core.StrictBytes b') = Aeson.decodeStrict' b'
+decodeFromUTF8 (Core.StrictBytes b') =
+  let
+    x :: Maybe Aeson.Value
+    x = Aeson.decodeStrict' b'
+  in
+    fmap fromAeson x
 
 data JsonValue
     = JsonObject (HashMap JsonKey JsonValue)
@@ -34,26 +44,61 @@ data JsonValue
     | JsonNull
     deriving (Eq, Read, Show, Generic)
 
-newtype JsonKey =
-    JsonKey Core.Text
+intoAeson :: JsonValue -> Aeson.Value
+intoAeson value = case value of
+    JsonObject xm ->
+        let
+            kvs = HashMap.toList xm
+            tvs = fmap (\(k, v) -> (Core.fromText (unJsonKey k), intoAeson v)) kvs
+            tvm :: HashMap T.Text Aeson.Value
+            tvm = HashMap.fromList tvs
+        in
+            Aeson.Object tvm
+
+    JsonArray xs ->
+        let
+            vs = fmap intoAeson xs
+        in
+            Aeson.Array (V.fromList vs)
+
+    JsonString x -> Aeson.String (Core.fromText x)
+    JsonNumber x -> Aeson.Number x
+    JsonBool x -> Aeson.Bool x
+    JsonNull -> Aeson.Null
+
+
+newtype JsonKey
+    = JsonKey Core.Text
     deriving (Eq, Show, Read, Generic, IsString)
+
+unJsonKey :: JsonKey -> Core.Text
+unJsonKey (JsonKey t) = t
 
 instance Hashable JsonKey
 
-instance Aeson.ToJSON JsonKey
-
-instance Aeson.ToJSONKey JsonKey
-
-instance Aeson.ToJSON JsonValue
-
 instance Aeson.ToJSON Core.Text where
-    toJSON = Aeson.toJSON . Core.intoText
+    toJSON (Core.UTF8 b') = error "No coding"
+    toJSON (Core.StrictText t) = Aeson.toJSON t
 
-instance Aeson.FromJSON JsonKey
+instance Core.Unicode JsonKey where
+    fromText t = Core.fromText t
+    intoText x = Core.intoText x
 
-instance Aeson.FromJSONKey JsonKey
 
-instance Aeson.FromJSON JsonValue
+fromAeson :: Aeson.Value -> JsonValue
+fromAeson value = case value of
+    Aeson.Object o ->
+        let
+            tvs = HashMap.toList o
+            kvs = fmap (\(k, v) -> (JsonKey (Core.intoText k), fromAeson v)) tvs
 
-instance Aeson.FromJSON Core.Text where
-    parseJSON = Aeson.parseJSON
+            kvm :: HashMap JsonKey JsonValue
+            kvm = HashMap.fromList kvs
+        in
+            JsonObject kvm
+
+    Aeson.Array v -> JsonArray (fmap fromAeson (V.toList v))
+    Aeson.String t -> JsonString (Core.intoText t)
+    Aeson.Number n -> JsonNumber n
+    Aeson.Bool x -> JsonBool x
+    Aeson.Null -> JsonNull
