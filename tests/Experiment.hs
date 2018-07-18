52bb42db62aa94ca097410f98cd2acc3b73eebc4
Andrew Cowie <andrew@operationaldynamics.com>
2018-07-18 21:44:52 +1000

    Add barebones test snippet

--- tests/Experiment.hs
+++ tests/Experiment.hs
@@ -1,8 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-top-binds #-}
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 
 import Data.Text (Text)
 import qualified Data.Text.IO as T
+import qualified Data.ByteString.Char8 as S
+import qualified Data.HashMap.Strict as HashMap
+
+import Core.Text
+import Core.Json
+
+k = JsonKey "name"
+v = JsonString "Hello"
+j = JsonObject (HashMap.fromList [(k, v)])
+
+b = StrictBytes (S.pack "{\"cost\": 4500}")
 
 main :: IO ()
 main = do
-    putStrLn "Done"
+    let x = encodeToUTF8 j
+    print x
+
+    let (Just y) = decodeFromUTF8 b
+    print y
+    print (encodeToUTF8 y)
