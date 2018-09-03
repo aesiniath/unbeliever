c64cf54cda248cbd7cdd8059bc130b4524faecce
Andrew Cowie <andrew@operationaldynamics.com>
2018-09-04 08:49:46 +1000

    Explore various shapes for configure types

--- tests/Experiment.hs
+++ tests/Experiment.hs
@@ -84,8 +84,179 @@ program = do
     terminate 0
 
 
+options =
+    [ Option "verbose" (Just 'v') [here|
+            Turn on event level logging to console. 
+            Valid values are "event", "debug", and "none" (the default
+            if you don't specify the verbose option).
+      |]
+    , Option "logging" Nothing [here|
+          Valid values are "normal", "structured", and ...
+      |]
+    , Option "logging" Nothing [here|
+          Valid values are "console", "file:/path/to/file.log", and "syslog"
+      |]
+    , Option "quiet" (Just 'q') [here|
+          Supress normal output.
+      |]
+    ]
+
+arguments =
+    [ Argument "input-file" "Main file to input"
+    ]
+
+environment =
+    [ Environment "CRAZY_MODE" "Specify how many crazies to activate"
+    , Environment "GITHUB_TOKEN" "OAuth token to access GitHub"
+    ]
+
+
 main :: IO ()
-main = execute program
+main = do
+    context <- configure (Config options arguments environment)
+    executeWith context program
+
+
+main2 :: IO ()
+main2 = do
+    context <- configure Config {
+        optionsFrom = [
+            Option "verbose" (Just 'v') [here|
+                Turn on event level logging to console. 
+                Valid values are "event", "debug", and "none" (the default
+                if you don't specify the verbose option).
+            |]
+          , Option "logging" Nothing [here|
+                Valid values are "normal", "structured", and ...
+            |]
+          , Option "logging" Nothing [here|
+                Valid values are "console", "file:/path/to/file.log", and "syslog"
+            |]
+
+          , Option "quiet" (Just 'q') [here|
+                Supress normal output.
+            |]
+          ]
+      , argumentsFrom = 
+          [  Argument "input-file" "Main file to input"
+          ]
+      , environmentFrom =
+          [ Environment "CRAZY_MODE" "Specify how many crazies to activate"
+          , Environment "GITHUB_TOKEN" "OAuth token to access GitHub"
+          ]
+      , commandsFrom =
+          [ Default
+              [ Argument "filename" "File to add"
+              ]
+          , Command "add" "Add a file" 
+              [ Argument "filename" "File to add"
+              ]
+
+          , Command "commit" "Commit your changes to the repository" 
+              [ Option "message" (Just 'm') "Specify commit message (instead of using editor)"
+              ]
+  
+      }
+
+    executeWith context program
+
+{-
+    Ok, so
+-}
+
+simple :: [Options] -> Config
+
+complex :: [Commands] -> Config
+
+data Commands 
+    = Global [Options]
+    | Command [Options]
+    | Environment [Variables]
+
+data Options
+    = Option
+    | Argument
+
+data Variables
+    = Variable
+
+{-
+    or
+-}
+
+data Commands 
+    = Global [Options]
+    | Command [Options]
+    | Environment [Options]
+
+data Options
+    = Option
+    | Argument
+    | Variable
+
+
+
+
+main3 :: IO ()
+main3 = do
+    context <- configure (simple
+            [ Option "verbose" (Just 'v') [here|
+                  Turn on event level logging to console. 
+                  Valid values are "event", "debug", and "none" (the default
+                  if you don't specify the verbose option).
+              |]
+            , Option "logging" Nothing [here|
+                  Valid values are "normal", "structured", and ...
+              |]
+            , Option "logging" Nothing [here|
+                  Valid values are "console", "file:/path/to/file.log", and "syslog"
+              |]
+            , Option "quiet" (Just 'q') [here|
+                  Supress normal output.
+              |]
+            ])
+  
+
+    context <- configure (complex
+        [ Global
+            [ Option "verbose" (Just 'v') [here|
+                  Turn on event level logging to console. 
+                  Valid values are "event", "debug", and "none" (the default
+                  if you don't specify the verbose option).
+              |]
+            , Option "logging" Nothing [here|
+                  Valid values are "normal", "structured", and ...
+              |]
+            , Option "logging" Nothing [here|
+                  Valid values are "console", "file:/path/to/file.log", and "syslog"
+              |]
+            , Option "quiet" (Just 'q') "Supress normal output."
+            ]
+
+        , Command "add" "Add a file" 
+            [ Argument "filename" "File to add"
+            ]
+
+        , Command "commit" "Commit your changes to the repository" 
+            [ Option "message" (Just 'm') "Specify commit message (instead of using editor)"
+            ]
+  
+        , Environment
+            [ Variable "CRAZY_MODE" "Specify how many crazies to activate"
+            , Variable "GITHUB_TOKEN" "OAuth token to access GitHub"
+            ]
+        ]
+  
+    executeWith context program
+
+main3 :: IO ()
+main3 = do
+    context <- configure $ do
+        options config
+        logging Structured
+
+
+
 
 {-
 main :: IO ()
@@ -97,3 +268,35 @@ main = do
     execute' context program
 -}
 
+main4 :: IO ()
+main4 = do
+    context <- configure (complex 
+        global $ do
+            Option "verbose" (Just 'v') [here|
+                Turn on event level logging to console. 
+                Valid values are "event", "debug", and "none" (the default
+                if you don't specify the verbose option).
+            |]
+            Option "logging" Nothing [here|
+                Valid values are "normal", "structured", and ...
+            |]
+            Option "logging" Nothing [here|
+                Valid values are "console", "file:/path/to/file.log", and "syslog"
+            |]
+            Option "quiet" (Just 'q') [here|
+                Supress normal output.
+            |]
+
+        command "add" "Add a file" (parameters [
+            Option "all" (Just 'u') "Add everything"
+            Arguments "filenames" "Files to add"
+        ]
+
+        command "commit" "Commit your changes to the repository" $ do
+            option "message" (Just 'm') "Specify commit message (instead of using editor)"
+
+        environment $ do
+            variable "CRAZY_MODE" "Specify how many crazies to activate"
+            variable "GITHUB_TOKEN" "OAuth token to access GitHub"
+  
+    executeWith context program
