e6ea947c31acbe876d51b7d9c664db5200ab9be2
Andrew Cowie <andrew@operationaldynamics.com>
2018-09-04 21:30:46 +1000

    New schema for specifying command line argument parsing

--- lib/Core/Program/Arguments.hs
+++ lib/Core/Program/Arguments.hs
@@ -2,11 +2,31 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
+--
+-- | Invoking a command-line program (be it tool or daemon) consists of
+-- listing the name of its binary, optionally supplying various
+-- options to adjust the behaviour of the program, and then supplying
+-- mandatory arguments, if any are specified.
+--
+-- On startup, we parse any arguments passed in from the shell into
+-- @name,value@ pairs and incorporated into the resultant configuration stored
+-- in the program's Context.
+--
+-- Additionally, this module allows you to specify environment variables that,
+-- if present, will be incorporated into the stored configuration.
+--
 module Core.Program.Arguments
     ( 
         Config
-      , minimalConfig
+      , baselineConfig
       , Parameters
+      , ParameterValue(..)
+      , simple
+      , complex
+      , Options(..)
+      , Commands(..)
+      , LongName(..)
+      , ShortName
       , parseCommandLine
     ) where
 
@@ -17,56 +37,56 @@ import Core.Text
 import Core.System
 import Core.Render
 
-{-
-    see Context for details of naming convention
--}
-
-data Config =
-    Config [Option] [Argument]
-
 type ShortName = Char
 
-newtype ParameterName = ParameterName String
+newtype LongName = LongName String
     deriving (Show, IsString)
 
-data Option = Option ParameterName (Maybe ShortName) Text
+data Config
+    = Simple [Options]
+    | Complex [Commands]
 
-data Argument = Argument ParameterName
+
+simple :: [Options] -> Config
+simple options = Simple options
+
+complex :: [Commands] -> Config
+complex commands = Complex commands
+
+data Commands 
+    = Global [Options]
+    | Command LongName Text [Options]
+    | Environment [Variables]
+
+data Options
+    = Option LongName (Maybe ShortName) Text
+    | Argument LongName
+
+data Variables
+    = Variable LongName Text
+
 
 newtype ParameterValue = ParameterValue String -- ugh
     deriving Show
 
-type Parameters = [(ParameterName, ParameterValue)]
+--
+-- Result of having processed the command line and the environment.
+--
+data Parameters
+    = Parameters {
+          commandNameFrom :: Maybe LongName
+        , parameterValuesFrom :: [(LongName, ParameterValue)]
+        , environmentValuesFrom :: [(LongName, ParameterValue)]
+    }
 
-minimalConfig :: Config
-minimalConfig = 
-    Config [
+baselineConfig :: Config
+baselineConfig =
+    simple [
         Option "verbose" (Just 'v') [here|
-            Turn on verbose logging. 
+            Turn on event level logging to console.
         |]
-        -- TODO "help"
+      , Option "help" (Just 'h') ""
     ]
-    []
 
-
-example :: Config
-example =
-    Config [
-        Option "verbose" (Just 'v') [here|
-            Turn on verbose logging. 
-        |]
-      , Option "crazy-mode-active" Nothing [here|
-            Do the crazy thing, once and for all.
-        |]
-    ]
-    [
-        Argument "input-file" 
-    ]
-{-
-        Environment "CRAZY_MODE" -- ?!?
--}
-
-
---parseCommandLine :: CommandLine -> [String] -> [(ParameterName, ParameterValue)]
 parseCommandLine :: Config -> [String] -> Parameters
-parseCommandLine config raw = [(ParameterName "verbose", ParameterValue "2")]
+parseCommandLine config raw = Parameters Nothing [(LongName "verbose", ParameterValue "2")] []
