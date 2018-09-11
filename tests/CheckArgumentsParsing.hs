004f2ec99cd34346a4a497ebeecb3d2e56a6d3e7
Andrew Cowie <andrew@operationaldynamics.com>
2018-09-12 08:50:08 +1000

    Further test cases for complex command lines

--- tests/CheckArgumentsParsing.hs
+++ tests/CheckArgumentsParsing.hs
@@ -4,9 +4,7 @@
 module CheckArgumentsParsing where
 
 import Test.Hspec
-import Control.Exception.Safe (throw)
 
-import Core.Text
 import Core.Program.Arguments
 
 options1 =
@@ -20,6 +18,11 @@ options2 =
     , Argument "filename" "The file that you want"
     ]
 
+options3 =
+    [ Option "all" (Just 'a') "Good will to everyone"
+    ]
+
+
 commands1 =
     [ Global
         options1
@@ -27,10 +30,19 @@ commands1 =
         options2
     ]
 
+commands2 =
+    [ Global
+        options1
+    , Command "add" "Add a new file"
+        options2
+    , Command "commit" "Commit for eternity"
+        options3
+    ]
+
 
 checkArgumentsParsing :: Spec
 checkArgumentsParsing = do
-    describe "Parsing of simple command-line arguments" $ do
+    describe "Parsing of simple command-lines" $ do
         it "recognizes a single specified options" $
           let
             config = simple options1
@@ -81,9 +93,9 @@ checkArgumentsParsing = do
           in
             actual `shouldBe` Left (MissingArgument "filename")
 
-    describe "Parsing of complex command-line arguments" $ do
+    describe "Parsing of complex command-lines" $ do
 
-        it "recognizes a single command" $
+        it "recognizes only single command" $
           let
             config = complex commands1
             actual = parseCommandLine config ["-q", "add", "--recursive", "Hello.hs"]
@@ -94,3 +106,34 @@ checkArgumentsParsing = do
                 ] []
           in
             actual `shouldBe` Right expect
+
+        it "fails on missing command" $
+          let
+            config = complex commands1
+            actual = parseCommandLine config []
+          in
+            actual `shouldBe` Left (NoCommandFound)
+
+        it "rejects an unknown command" $
+          let
+            config = complex commands1
+            actual = parseCommandLine config ["launch"]
+          in
+            actual `shouldBe` Left (UnknownCommand "launch")
+
+        it "recognizes different command" $ -- ie, now from among multiple choices
+          let
+            config = complex commands2
+            actual = parseCommandLine config ["commit"]
+            expect = Parameters (Just "commit") [] []
+          in
+            actual `shouldBe` Right expect
+
+
+        it "rejects further trailing arguments" $
+          let
+            config = complex commands2
+            actual = parseCommandLine config ["commit", "some"]
+          in
+            actual `shouldBe` Left (UnexpectedArguments ["some"])
+
