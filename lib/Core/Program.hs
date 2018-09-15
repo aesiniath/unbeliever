b53887a2e331adf81ffd1f3abcffef17bc1dd26c
Andrew Cowie <andrew@operationaldynamics.com>
2018-09-15 23:54:14 +1000

    Document top level Core.Program module

--- lib/Core/Program.hs
+++ lib/Core/Program.hs
@@ -1,14 +1,35 @@
 {-# OPTIONS_HADDOCK not-home #-}
 
 {-|
-Support for building command-line programs, ranging from simple tools
-to long-running daemons.
+Support for building command-line programs, ranging from simple tools to
+long-running daemons.
+
+This is intended to be used directly:
+
+@
+import "Core.Program"
+@
+
+the submodules are mostly there to group documentation.
 -}
+-- actually, they're there to group implementation too, but hey.
 module Core.Program
     (
-      module Core.Program.Arguments
-    , module Core.Program.Logging
-    , module Core.Program.Execute
+        {-* Executing a program -}
+{-|
+A top-level Program type giving you unified access to logging, concurrency,
+and more.
+-}
+        module Core.Program.Execute
+
+        {-* Command-line argument parsing -}
+{-|
+Including declaring what options your program accepts, generating help, and
+for more complex cases [sub]commands, mandatory arguments, and environment
+variable handling.
+-}
+      , module Core.Program.Arguments
+      , module Core.Program.Logging
     ) where
 
 import Core.Program.Arguments
