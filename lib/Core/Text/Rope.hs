cc8416fd571e0ddc0882f5ed54b68e477617f773
Andrew Cowie <andrew@operationaldynamics.com>
2018-09-25 20:14:06 +1000

    Documentation of background to use of a rope for text

--- lib/Core/Text/Rope.hs
+++ lib/Core/Text/Rope.hs
@@ -6,6 +6,69 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE InstanceSigs #-}
 
+{-|
+If you're accustomed to working with text in almost any other programming
+language, you'd be aware that a \"string\" typically refers to an in-memory
+/array/ of characters. Traditionally this was a single ASCII byte per
+character; more recently UTF-8 variable byte encodings which dramatically
+complicates finding offsets but which gives efficient support for the
+entire Unicode character space. In Haskell, the original text type,
+'String', is implemented as a list of 'Char' which, because a Haskell list
+is implemented as a /linked-list of boxed values/, is wildly inefficient at
+any kind of scale.
+
+In modern Haskell there are two primary ways to represent text.
+
+First is via the [rather poorly named] 'ByteString' from the __bytestring__
+package (which is an array of bytes in pinned memory). The
+"Data.ByteString.Char8" submodule gives you ways to manipulate those arrays
+as if they were ASCII characters. Confusingly there are both strict
+(@Data.ByteString@) and lazy (@Data.ByteString.Lazy@) variants which are
+often hard to tell the difference between when reading function signatures
+or haddock documentation. The performance problem an immutable array backed
+data type runs into is that appending a character (that is, ascii byte) or
+concatonating a string (that is, another array of ascii bytes) is very
+expensive and requires allocating a new larger array and copying the whole
+thing into it. This led to the development of \"Builders\" which amortize
+this reallocation cost over time, but it can be cumbersome to switch
+between 'Builder', the lazy 'Data.ByteString.Lazy.ByteString' that results,
+and then having to inevitably convert to a strict 'ByteString' because
+that's what the next function in your sequence requires.
+
+The second way is through the opaque 'Text' type of "Data.Text" from the
+__text__ package, which is well tuned and high-performing but suffers from
+the same design; it is likewise backed by arrays. Rather surprisingly, the
+storage backing Text objects are encoded in UTF-16, meaning every time you
+want to work with unicode characters that came in from /anywhere/ else and
+which inevitably are UTF-8 encoded you have to convert to UTF-16 and copy
+into a new array, wasting time and memory.
+
+In this package we introduce 'Rope', a text type backed by the 2-3
+'Data.FingerTree.FingerTree' data structure from the __fingertree__
+package. This is not an uncommon solution in many languages as finger trees
+support exceptionally efficient appending to either end and good
+performance inserting anywhere else (you often find them as the backing
+data type underneath text editors for this reason). Rather than 'Char' the
+pieces of the rope are 'Data.Text.Short.ShortText' from the __text-short__
+package, which are UTF-8 encoded and in normal memory managed by the
+Haskell runtime. Conversion from other Haskell text types is not /O(1)/
+(UTF-8 validity must be checked, or UTF-16 decoded, or...), but in our
+benchmarking the performance has been comparable to the established types
+and you may find the resultant interface for combining chunks is comparable
+to using a Builder, without being forced to use a Builder.
+
+'Rope' is used as the text type throughout this library. If you use the
+functions within this package (rather than converting to other text types)
+operations are very efficient. When you do need to convert to another type
+you can use 'fromRope' or 'intoRope' from the 'Textual' typeclass.
+
+Note that we haven't tried to cover the entire gamut of operations or
+customary convenience functions you would find in the other libraries; so
+far 'Rope' is concentrated on being good at appending (lots of) small
+pieces, and in taking the resultant text object out to a file handle, be
+that the terminal console, a file, or a network socket.
+
+-}
 module Core.Text.Rope
     ( Rope
     , Width(..)
@@ -40,6 +103,7 @@ import GHC.Generics (Generic)
 import System.IO (Handle)
 
 {-|
+A type for textual data.
 
 There are two use cases: first, referencing large blocks of data sourced from
 external systems. Ideally we would hold onto this without copying the memory.
