b5e648f8c5a92c8bcfdd97b14f43537e7e7e0d93
Andrew Cowie <andrew@operationaldynamics.com>
2018-09-28 17:37:41 +1000

    Introduce second Core.System module for external dependencies

---
+++ lib/Core/System/External.hs
@@ -0,0 +1,15 @@
+{-# OPTIONS_HADDOCK not-home #-}
+
+--
+-- | Re-exports of dependencies from various external libraries.
+--
+module Core.System.External
+    ( {-* Time -}
+      {-** from Chrono.TimeStamp -}
+      {-| Re-exported from "Chrono.TimeStamp" in __chronologique: -}
+      TimeStamp(..)
+    , getCurrentTimeNanoseconds
+    ) where
+
+import Chrono.TimeStamp (TimeStamp, getCurrentTimeNanoseconds)
+
