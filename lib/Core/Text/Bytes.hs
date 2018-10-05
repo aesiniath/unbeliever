23a21cfa859d3a27177306071fc0acf6f4401cce
Andrew Cowie <andrew@operationaldynamics.com>
2018-10-05 21:08:32 +1000

    Initial documentation in Core.Text.Bytes

--- lib/Core/Text/Bytes.hs
+++ lib/Core/Text/Bytes.hs
@@ -5,8 +5,26 @@
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}        -- FIXME
 {-# OPTIONS_GHC -fno-warn-incomplete-patterns #-}   -- FIXME
 
+{-|
+Binary (as opposed to textual) data is encountered in weird corners of the
+Haskell ecosystem. We tend to forget (for example) that the content
+recieved from a web server is /not/ text until we convert it from UTF-8 (if
+that's what it is); and of course that glosses over the fact that something
+of content-type @image/jpeg@ is not text in any way, shape, or form.
+
+Bytes also show up when working with crypto algorithms, taking hashes, and
+when doing serialization to external binary formats. Although we frequently
+display these in terminals (and in URLs!) as text, but we take for granted
+that we have actually deserialized the data or rendered the it in
+hexidecimal or base64 or...
+
+This module presents a simple wrapper around various representations of
+binary data to make it easier to interoperate with libraries supplying
+or consuming bytes.
+-}
 module Core.Text.Bytes
     ( Bytes(..)
+    , Binary
     , fromBytes
     , intoBytes
     ) where
@@ -17,6 +35,9 @@ import Data.Hashable (Hashable)
 import Data.Word (Word8)
 import GHC.Generics (Generic)
 
+{-|
+A block of data in binary form.
+-}
 data Bytes
     = StrictBytes S.ByteString
     | LazyBytes L.ByteString
@@ -26,6 +47,9 @@ data Bytes
 {-|
 Conversion to and from various types containing binary data into our
 convenience Bytes type.
+
+As often as not these conversions are /expensive/; these methods are
+here just to wrap calling the relevant functions in a uniform interface.
 -}
 class Binary α where
     fromBytes :: Bytes -> α
