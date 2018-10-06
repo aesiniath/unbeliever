fd54ae7dcd65406707df10e52975d5e80de22064
Andrew Cowie <andrew@operationaldynamics.com>
2018-10-06 20:26:30 +1000

    Initial attempt at Render instance for Bytes

--- lib/Core/Text/Bytes.hs
+++ lib/Core/Text/Bytes.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE StrictData #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE BangPatterns #-}
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}        -- FIXME
 {-# OPTIONS_GHC -fno-warn-incomplete-patterns #-}   -- FIXME
 
@@ -24,22 +26,30 @@ or consuming bytes.
 -}
 module Core.Text.Bytes
     ( Bytes(..)
-    , Binary
-    , fromBytes
-    , intoBytes
+    , Binary(fromBytes, intoBytes)
     ) where
 
-import qualified Data.ByteString as S (ByteString)
+import Data.Bits (Bits (..))
+import Data.Char (intToDigit)
+import qualified Data.ByteString as B (ByteString, foldl')
+import Data.ByteString.Internal (c2w, w2c)
 import qualified Data.ByteString.Lazy as L (ByteString, fromStrict, toStrict)
 import Data.Hashable (Hashable)
 import Data.Word (Word8)
 import GHC.Generics (Generic)
+import Data.Text.Prettyprint.Doc
+    ( Doc, emptyDoc, pretty, annotate, (<+>), line, softline
+    )
+import Data.Text.Prettyprint.Doc.Render.Terminal (
+    color, colorDull, bold, Color(..))
+
+import Core.Text.Utilities
 
 {-|
 A block of data in binary form.
 -}
 data Bytes
-    = StrictBytes S.ByteString
+    = StrictBytes B.ByteString
     | LazyBytes L.ByteString
     | ListBytes [Word8]
     deriving (Show, Eq)
@@ -55,7 +65,7 @@ class Binary α where
     fromBytes :: Bytes -> α
     intoBytes :: α -> Bytes
 
-instance Binary S.ByteString where
+instance Binary B.ByteString where
     fromBytes (StrictBytes b') = b'
     intoBytes b' = StrictBytes b'
 
@@ -63,6 +73,28 @@ instance Binary L.ByteString where
     fromBytes (StrictBytes b') = L.fromStrict b'
     intoBytes b' = StrictBytes (L.toStrict b')      -- expensive
 
+-- (), aka Unit, aka **1**, aka something with only one inhabitant
+
+instance Render Bytes where
+    type Token Bytes = ()
+    colourize = const (color Green)
+    intoDocA (StrictBytes b') = annotate () . snd . B.foldl' f (-1,emptyDoc) $ b'
+      where
+        f :: (Int, Doc ann) -> Word8 -> (Int, Doc ann)
+        f (-1, _ ) w = (1, byteToHex w)
+        f (0, acc) w = (1, acc <+> softline <> byteToHex w)
+        f (!i,acc) w = case i of
+            7 -> (0,   acc <+> byteToHex w)
+            _ -> (i+1, acc <+> byteToHex w)
+
+byteToHex :: Word8 -> Doc ann
+byteToHex c = pretty hi <> pretty low
+  where
+    !low      = byteToDigit $ c .&. 0xf
+    !hi       = byteToDigit $ (c .&. 0xf0) `shiftR` 4
+
+    byteToDigit :: Word8 -> Char
+    byteToDigit = intToDigit . fromIntegral
 
 {-
 instance Show Bytes where
