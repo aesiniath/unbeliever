58c93a1471d64e5c92af13d7fea693d55a7ec13d
Andrew Cowie <andrew@operationaldynamics.com>
2018-11-29 22:20:38 +1100

    Add Dictionary instance for [Ord] Maps

--- lib/Core/Data/Structures.hs
+++ lib/Core/Data/Structures.hs
@@ -33,6 +33,7 @@ on `lookup1` because brevity.
 
 import Data.Hashable (Hashable)
 import qualified Data.HashMap.Strict as Unordered
+import qualified Data.Map.Strict as Containers
 import qualified Data.Text as T (Text)
 import qualified Data.Text.Lazy as U (Text)
 import GHC.Exts (IsList(..))
@@ -95,11 +96,15 @@ instance Key κ => IsList (Map κ ν) where
 type Set κ = Map κ ()
 
 --class Dictionary α where
-class Dictionary α κ ν where
+class Key κ => Dictionary α κ ν where
     fromMap :: Map κ ν -> α κ ν
     intoMap :: α κ ν -> Map κ ν
 
-instance Dictionary Unordered.HashMap κ ν where
+instance Key κ => Dictionary Unordered.HashMap κ ν where
     fromMap (Map p) = p
     intoMap p = Map p
+
+instance Key κ => Dictionary Containers.Map κ ν where
+    fromMap (Map p) = Containers.fromList (Unordered.toList p)
+    intoMap o = Map (Unordered.fromList (Containers.toList o))
 
