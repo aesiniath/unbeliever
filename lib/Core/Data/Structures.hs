3903040e32eb8ee1bd3471a048b9ca7af9967574
Andrew Cowie <andrew@operationaldynamics.com>
2018-12-02 15:21:58 +1100

    Change name used for qualified imports

--- lib/Core/Data/Structures.hs
+++ lib/Core/Data/Structures.hs
@@ -24,8 +24,9 @@ where
 
 import Data.Foldable (Foldable(..))
 import Data.Hashable (Hashable)
-import qualified Data.HashMap.Strict as Unordered
-import qualified Data.Map.Strict as Containers
+import qualified Data.HashMap.Strict as HashMap
+import qualified Data.HashSet as HashSet
+import qualified Data.Map.Strict as OrdMap
 import qualified Data.Text as T (Text)
 import qualified Data.Text.Lazy as U (Text)
 import qualified GHC.Exts as Exts (IsList(..))
@@ -33,10 +34,10 @@ import qualified GHC.Exts as Exts (IsList(..))
 import Core.Text.Rope (Rope)
 import Core.Text.Bytes (Bytes)
 
-newtype Map κ ν = Map (Unordered.HashMap κ ν)
+newtype Map κ ν = Map (HashMap.HashMap κ ν)
     deriving (Show, Eq)
 
-unMap :: Map κ ν -> Unordered.HashMap κ ν
+unMap :: Map κ ν -> HashMap.HashMap κ ν
 unMap (Map p) = p
 {-# INLINE unMap #-}
 
@@ -50,29 +51,29 @@ instance Key U.Text
 instance Key Char
 
 instance Foldable (Map κ) where
-    foldr f start (Map p) = Unordered.foldr f start p
-    null (Map p) = Unordered.null p
-    length (Map p) = Unordered.size p
+    foldr f start (Map p) = HashMap.foldr f start p
+    null (Map p) = HashMap.null p
+    length (Map p) = HashMap.size p
 
 emptyMap :: Map κ ν
-emptyMap = Map (Unordered.empty)
+emptyMap = Map (HashMap.empty)
 
 singletonMap :: Key κ => κ -> ν -> Map κ ν
-singletonMap k v = Map (Unordered.singleton k v)
+singletonMap k v = Map (HashMap.singleton k v)
 
 insertKeyValue :: Key κ => κ -> ν -> Map κ ν -> Map κ ν
-insertKeyValue k v (Map p) = Map (Unordered.insert k v p)
+insertKeyValue k v (Map p) = Map (HashMap.insert k v p)
 
 lookupKeyValue :: Key κ => κ -> Map κ ν -> Maybe ν
-lookupKeyValue k (Map p) = Unordered.lookup k p
+lookupKeyValue k (Map p) = HashMap.lookup k p
 
 containsKey :: Key κ => κ -> Map κ ν -> Bool
-containsKey k (Map p) = Unordered.member k p
+containsKey k (Map p) = HashMap.member k p
 
 {-|
 -}
 instance Key κ => Semigroup (Map κ ν) where
-    (<>) (Map p1) (Map p2) = Map (Unordered.union p1 p2)
+    (<>) (Map p1) (Map p2) = Map (HashMap.union p1 p2)
 
 instance Key κ => Monoid (Map κ ν) where
     mempty = emptyMap
@@ -80,10 +81,8 @@ instance Key κ => Monoid (Map κ ν) where
 
 instance Key κ => Exts.IsList (Map κ ν) where
     type Item (Map κ ν) = (κ, ν)
-    fromList pairs = Map (Unordered.fromList pairs)
-    toList (Map p) = Unordered.toList p
-
-type Set κ = Map κ ()
+    fromList pairs = Map (HashMap.fromList pairs)
+    toList (Map p) = HashMap.toList p
 
 {-|
 Types that represent key/value pairs that can be converted to 'Map's.
@@ -125,18 +124,18 @@ class Dictionary α where
     intoMap :: α -> Map (K α) (V α)
 
 {-| from "Data.HashMap.Strict" -}
-instance Key κ => Dictionary (Unordered.HashMap κ ν)  where
-    type K (Unordered.HashMap κ ν) = κ
-    type V (Unordered.HashMap κ ν) = ν
+instance Key κ => Dictionary (HashMap.HashMap κ ν) where
+    type K (HashMap.HashMap κ ν) = κ
+    type V (HashMap.HashMap κ ν) = ν
     fromMap (Map p) = p
     intoMap p = Map p
 
 {-| from "Data.Map.Strict" -}
-instance Key κ => Dictionary (Containers.Map κ ν) where
-    type K (Containers.Map κ ν) = κ
-    type V (Containers.Map κ ν) = ν
-    fromMap (Map p) = Containers.fromList (Unordered.toList p)
-    intoMap o = Map (Unordered.fromList (Containers.toList o))
+instance Key κ => Dictionary (OrdMap.Map κ ν) where
+    type K (OrdMap.Map κ ν) = κ
+    type V (OrdMap.Map κ ν) = ν
+    fromMap (Map p) = OrdMap.fromList (HashMap.toList p)
+    intoMap o = Map (HashMap.fromList (OrdMap.toList o))
 
 instance Key κ => Dictionary [(κ,ν)] where
     type K [(κ,ν)] = κ
