505675d0279bc976d82ab9dd3cc39b23666ecf25
Andrew Cowie <andrew@operationaldynamics.com>
2019-07-08 23:39:10 +1000

    Split package into n subpackages
    Preliminary effort refactoring this package into numerous sub-packages.
    The objective is to enable the individual components to be used without
    incurring the massive dependency tree that comes with a
    batteries-included monolithic package.

---
+++ lib/Core.hs
@@ -0,0 +1,19 @@
+{-# OPTIONS_HADDOCK not-home #-}
+
+{-|
+-}
+module Core
+    (
+        module Core.Text
+      , module Core.Program
+      , module Core.System
+      , module Core.Data
+      , module Core.Encoding
+    ) where
+
+import Core.Text
+import Core.Program
+import Core.System
+import Core.Data
+import Core.Encoding
+
