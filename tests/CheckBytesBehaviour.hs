8361bc0629461aa6c1d19dc8fd3f4876ba3de7fe
Andrew Cowie <andrew@operationaldynamics.com>
2019-10-25 21:56:56 +1100

    Clear warning
    Clearly the Bytes type needs more tests.

--- tests/CheckBytesBehaviour.hs
+++ tests/CheckBytesBehaviour.hs
@@ -7,7 +7,7 @@ module CheckBytesBehaviour where
 import qualified Data.ByteString.Char8 as C
 import Test.Hspec
 
-import Core.Text.Bytes
+import Core.Text.Bytes ()
 import Core.Text.Utilities (byteChunk)
 
 checkBytesBehaviour :: Spec
