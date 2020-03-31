bc68afe84e5a033cc905d0c0f6b84cf27c30deb3
Andrew Cowie <andrew@operationaldynamics.com>
2020-03-31 20:21:41 +1100

    Document testProgram function

--- core-verify/lib/Core/Testing/Support.hs
+++ core-verify/lib/Core/Testing/Support.hs
@@ -4,8 +4,9 @@
 {-# OPTIONS_HADDOCK prune #-}
 
 {-|
-Support for testing programs written in the 'Program' @t@ type without acutally
-standing up the full stand-alone application execution environment.
+Support for testing programs written in the 'Program' @τ@ type without
+acutally standing up the full stand-alone application execution
+environment.
 -}
 module Core.Testing.Support
     (   testProgram
@@ -15,6 +16,17 @@ import Core.Program.Execute (Program, configure)
 import Core.Program.Arguments (blank)
 import Core.Program.Unlift (subProgram)
 
+{-|
+Run a 'Program' @τ@ and return the result.
+
+This sets up a minimal context that will not actually output or log
+anything but which can be used to evaluate a Program action in 
+
+/This is preliminary; while the function signature will remain stable we/
+/have considerable work to do for this to be idempotent, reusable, and/
+/safe./
+-}
+-- TODO better isolation! Calling configure does way to much actual work.
 testProgram :: forall α τ. τ -> Program τ α -> IO α
 testProgram user program = do
     context <- configure "0" user blank
