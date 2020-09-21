d206bd50c1ec529169faf92a95e147bbc7ed03f9
Andrew Cowie <istathar@gmail.com>
2020-09-21 13:42:11 +1000

    Format code with Ormolu

--- core-program/lib/Core/Program.hs
+++ core-program/lib/Core/Program.hs
@@ -1,49 +1,48 @@
 {-# OPTIONS_HADDOCK not-home #-}
 
-{-|
-Support for building command-line programs, ranging from simple tools to
-long-running daemons.
-
-This is intended to be used directly:
-
-@
-import "Core.Program"
-@
-
-the submodules are mostly there to group documentation.
--}
 -- actually, they're there to group implementation too, but hey.
+
+-- |
+-- Support for building command-line programs, ranging from simple tools to
+-- long-running daemons.
+--
+-- This is intended to be used directly:
+--
+-- @
+-- import "Core.Program"
+-- @
+--
+-- the submodules are mostly there to group documentation.
 module Core.Program
-    (
-        {-* Executing a program -}
-{-|
-A top-level Program type giving you unified access to logging, concurrency,
-and more.
--}
-        module Core.Program.Execute
-      , module Core.Program.Unlift
-      , module Core.Program.Metadata
+  ( -- * Executing a program
 
-        {-* Command-line argument parsing -}
-{-|
-Including declaring what options your program accepts, generating help, and
-for more complex cases [sub]commands, mandatory arguments, and environment
-variable handling.
--}
-      , module Core.Program.Arguments
-        {-* Logging facilities -}
-{-|
-Facilities for noting events through your program and doing debugging.
--}
-      , module Core.Program.Logging
+    -- |
+    -- A top-level Program type giving you unified access to logging, concurrency,
+    -- and more.
+    module Core.Program.Execute,
+    module Core.Program.Unlift,
+    module Core.Program.Metadata,
 
-{-|
-There are a few common use cases which require a bit of wrapping to use
-effectively. Watching files for changes and taking action in the event of a
-change is one.
--}
-      , module Core.Program.Notify
-    ) where
+    -- * Command-line argument parsing
+
+    -- |
+    -- Including declaring what options your program accepts, generating help, and
+    -- for more complex cases [sub]commands, mandatory arguments, and environment
+    -- variable handling.
+    module Core.Program.Arguments,
+
+    -- * Logging facilities
+
+    -- |
+    -- Facilities for noting events through your program and doing debugging.
+    module Core.Program.Logging,
+    -- |
+    -- There are a few common use cases which require a bit of wrapping to use
+    -- effectively. Watching files for changes and taking action in the event of a
+    -- change is one.
+    module Core.Program.Notify,
+  )
+where
 
 import Core.Program.Arguments
 import Core.Program.Execute
@@ -51,4 +50,3 @@ import Core.Program.Logging
 import Core.Program.Metadata
 import Core.Program.Notify
 import Core.Program.Unlift
-
--- core-program/lib/Core/System.hs
+++ core-program/lib/Core/System.hs
@@ -1,51 +1,47 @@
 {-# OPTIONS_HADDOCK not-home #-}
 
-{-|
-Common elements from the rest of the Haskell ecosystem. This is mostly
-about re-exports. There are numerous types and functions that are more or
-less assumed to be in scope when you're doing much of anything in Haskell;
-this module is a convenience to pull in the ones we rely on for the rest of
-this library.
-
-You can just import this directly:
-
-@
-import "Core.System"
-@
-
-as there's no particular benefit to cherry-picking the various sub-modules.
-
--}
+-- |
+-- Common elements from the rest of the Haskell ecosystem. This is mostly
+-- about re-exports. There are numerous types and functions that are more or
+-- less assumed to be in scope when you're doing much of anything in Haskell;
+-- this module is a convenience to pull in the ones we rely on for the rest of
+-- this library.
+--
+-- You can just import this directly:
+--
+-- @
+-- import "Core.System"
+-- @
+--
+-- as there's no particular benefit to cherry-picking the various sub-modules.
 module Core.System
-    (
-        {-* Base libraries -}
-{-|
-Re-exports from foundational libraries supplied by the compiler runtime,
-or from re-implementations of those areas.
--}
-        module Core.System.Base
+  ( -- * Base libraries
 
-        {-* External dependencies -}
-{-|
-Dependencies from libraries outside the traditional ecosystem of Haskell.
-These are typically special cases or custom re-implementations of things
-which are maintained either by ourselves or people we are in regular
-contact with.
--}
-      , module Core.System.External
+    -- |
+    -- Re-exports from foundational libraries supplied by the compiler runtime,
+    -- or from re-implementations of those areas.
+    module Core.System.Base,
 
-        {-* Pretty Printing -}
-{-|
-When using the Render typeclass from "Core.Text.Utilities" you are
-presented with the @Doc a@ type for accumulating a \"document\" to be
-pretty printed. There are a large family of combinators used when doing
-this. For convenience they are exposed here.
--}
-      , module Core.System.Pretty
+    -- * External dependencies
 
-    ) where
+    -- |
+    -- Dependencies from libraries outside the traditional ecosystem of Haskell.
+    -- These are typically special cases or custom re-implementations of things
+    -- which are maintained either by ourselves or people we are in regular
+    -- contact with.
+    module Core.System.External,
+
+    -- * Pretty Printing
+
+    -- |
+    -- When using the Render typeclass from "Core.Text.Utilities" you are
+    -- presented with the @Doc a@ type for accumulating a \"document\" to be
+    -- pretty printed. There are a large family of combinators used when doing
+    -- this. For convenience they are exposed here.
+    module Core.System.Pretty,
+  )
+where
 
 import Core.System.Base
 import Core.System.External
 import Core.System.Pretty
-
