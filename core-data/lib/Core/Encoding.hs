2835e58e89a0ed72ddf34929825f9c42524464ae
Andrew Cowie <istathar@gmail.com>
2021-02-28 14:57:35 +1100

    Format code with Fourmolu

--- core-data/lib/Core/Encoding.hs
+++ core-data/lib/Core/Encoding.hs
@@ -1,23 +1,23 @@
 {-# OPTIONS_HADDOCK not-home #-}
 
--- |
--- Various formats used for serialization, data transfer, and configuration.
---
--- This can be used by simply importing the top level module:
---
--- @
--- import "Core.Encoding"
--- @
---
--- although the individual formats are quite usable indepedently.
---
--- Each of these encodings are backed by a popular and well tuned library in
--- wide use across the Haskell community; these modules are here as wrappers
--- providing for ease of use and interoperability across the various tools in
--- this package.
-module Core.Encoding
-  ( module Core.Encoding.Json,
-  )
-where
+{- |
+Various formats used for serialization, data transfer, and configuration.
+
+This can be used by simply importing the top level module:
+
+@
+import "Core.Encoding"
+@
+
+although the individual formats are quite usable indepedently.
+
+Each of these encodings are backed by a popular and well tuned library in
+wide use across the Haskell community; these modules are here as wrappers
+providing for ease of use and interoperability across the various tools in
+this package.
+-}
+module Core.Encoding (
+    module Core.Encoding.Json,
+) where
 
 import Core.Encoding.Json
