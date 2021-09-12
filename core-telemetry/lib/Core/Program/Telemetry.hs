5b7134fde4b05591612fc4b7f9a6cbc9772cd620
Andrew Cowie <istathar@gmail.com>
2021-09-12 23:13:47 +1000

    Stub functions for events and spans

--- core-telemetry/lib/Core/Program/Telemetry.hs
+++ core-telemetry/lib/Core/Program/Telemetry.hs
@@ -1,3 +1,22 @@
-module Core.Program.Telemetry where
+module Core.Program.Telemetry (
+    eventT,
+    debugT,
+    Telemetry (..),
+    withSpan,
+) where
 
+import Core.Program.Execute
+import Core.Text.Bytes
+import Core.Text.Rope
 
+eventT :: Telemetry a => a -> Program z ()
+eventT = undefined
+
+debugT :: Telemetry a => Rope -> a -> Program z ()
+debugT = undefined
+
+class Telemetry a where
+    serialize :: a -> Bytes
+
+withSpan :: Rope -> Program t a -> Program t a
+withSpan = undefined
