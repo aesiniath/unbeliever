08496f2f5328aa11ea3e92a999ec8d8f04758cf6
Andrew Cowie <istathar@gmail.com>
2021-09-21 12:26:47 +1000

    Add Telemetry instances for numeric and textual types

--- core-telemetry/lib/Core/Program/Telemetry.hs
+++ core-telemetry/lib/Core/Program/Telemetry.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
 
 module Core.Program.Telemetry (
@@ -17,6 +18,12 @@ module Core.Program.Telemetry (
 import Core.Encoding.Json
 import Core.Program.Execute
 import Core.Text.Rope
+import qualified Data.ByteString as B (ByteString)
+import qualified Data.ByteString.Lazy as L (ByteString)
+import Data.Int (Int32, Int64)
+import Data.Scientific (Scientific)
+import qualified Data.Text as T (Text)
+import qualified Data.Text.Lazy as U (Text)
 
 eventT :: Telemetry σ => σ -> Program τ ()
 eventT = undefined
@@ -39,14 +46,51 @@ class Telemetry σ where
 instance Telemetry Int where
     metric k v = MetricValue (JsonKey k) (JsonNumber (fromIntegral v))
 
+instance Telemetry Int32 where
+    metric k v = MetricValue (JsonKey k) (JsonNumber (fromIntegral v))
+
+instance Telemetry Int64 where
+    metric k v = MetricValue (JsonKey k) (JsonNumber (fromIntegral v))
+
+instance Telemetry Integer where
+    metric k v = MetricValue (JsonKey k) (JsonNumber (fromInteger v))
+
+-- is this the efficient way to get to a Scientific?
 instance Telemetry Float where
     metric k v = MetricValue (JsonKey k) (JsonNumber (fromRational (toRational v)))
 
+-- is this the efficient way to get to a Scientific?
 instance Telemetry Double where
     metric k v = MetricValue (JsonKey k) (JsonNumber (fromRational (toRational v)))
 
+instance Telemetry Scientific where
+    metric k v = MetricValue (JsonKey k) (JsonNumber v)
+
 instance Telemetry Rope where
     metric k v = MetricValue (JsonKey k) (JsonString v)
+
+instance Telemetry String where
+    metric k v = MetricValue (JsonKey k) (JsonString (intoRope v))
+
+{- |
+The usual warning about assuming the @ByteString@ is ASCII or UTF-8 applies
+here. Don't use this to send binary mush.
+-}
+instance Telemetry B.ByteString where
+    metric k v = MetricValue (JsonKey k) (JsonString (intoRope v))
+
+{- |
+The usual warning about assuming the @ByteString@ is ASCII or UTF-8 applies
+here. Don't use this to send binary mush.
+-}
+instance Telemetry L.ByteString where
+    metric k v = MetricValue (JsonKey k) (JsonString (intoRope v))
+
+instance Telemetry T.Text where
+    metric k v = MetricValue (JsonKey k) (JsonString (intoRope v))
+
+instance Telemetry U.Text where
+    metric k v = MetricValue (JsonKey k) (JsonString (intoRope v))
 
 instance Telemetry Bool where
     metric k v = MetricValue (JsonKey k) (JsonBool v)
