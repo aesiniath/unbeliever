6533426974d5be6ef9b6f4bfa8476b3fdd87afec
Andrew Cowie <istathar@gmail.com>
2021-09-26 00:50:25 +1000

    Derive MonadMask instance
    It also turns out we can derive MonadCatch rather than hand writing it.
    Huzzah.

--- core-program/lib/Core/Program/Context.hs
+++ core-program/lib/Core/Program/Context.hs
@@ -220,20 +220,9 @@ wrap/export **safe-exceptions**'s variants of the functions.
 instance MonadThrow (Program τ) where
     throwM = liftIO . Safe.throw
 
-unHandler :: (ε -> Program τ α) -> (ε -> ReaderT (Context τ) IO α)
-unHandler = fmap unProgram
+deriving instance MonadCatch (Program τ)
 
-instance MonadCatch (Program τ) where
-    catch :: Exception ε => (Program τ) α -> (ε -> (Program τ) α) -> (Program τ) α
-    catch program handler =
-        let r = unProgram program
-            h = unHandler handler
-         in do
-                context <- ask
-                liftIO $ do
-                    Safe.catch
-                        (runReaderT r context)
-                        (\e -> runReaderT (h e) context)
+deriving instance MonadMask (Program t)
 
 {- |
 Initialize the programs's execution context. This takes care of various
