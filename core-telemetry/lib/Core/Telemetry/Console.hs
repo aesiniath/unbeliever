2ad0c021a3259534e4da2d475da2e41b615112ee
Andrew Cowie <istathar@gmail.com>
2021-10-10 14:05:18 +1100

    Separate backend exporters into own modules

---
+++ core-telemetry/lib/Core/Telemetry/Console.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralisedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+{- |
+Implementations of different backends that telemetry can be exported to.
+-}
+module Core.Telemetry.Console (
+    Exporter,
+    consoleExporter,
+) where
+
+import Core.Data.Structures (fromMap)
+import Core.Encoding.Json
+import Core.Program.Context
+import Core.Program.Logging
+import Core.System.External (getCurrentTimeNanoseconds)
+import Core.Text.Colour
+import Core.Text.Rope
+import Core.Text.Utilities
+import qualified Data.List as List
+
+-- TODO convert this into a Render instance
+
+-- Somewhat counterintuitively, this does NOT do I/O, but instead returns text
+-- which `processTelemetryMessages` will then forward to the main output queue
+-- consumed by `processStandardOutput`. This is a bit roundabout, but ensures
+-- debug output from this function doesn't smash the console.
+consoleExporter :: Exporter
+consoleExporter =
+    Exporter
+        { codenameFrom = "console"
+        , processorFrom = process
+        }
+  where
+    process :: Datum -> IO Rope
+    process datum = do
+        now <- getCurrentTimeNanoseconds
+        let start = spanTimeFrom datum
+        let text =
+                (intoEscapes pureGrey)
+                    <> spanNameFrom datum
+                    <> " metrics:"
+                    <> let pairs :: [(JsonKey, JsonValue)]
+                           pairs = fromMap (attachedMetadataFrom datum)
+                        in List.foldl' f emptyRope pairs
+                            <> (intoEscapes resetColour)
+
+        let result = formatLogMessage start now SeverityDebug text
+        pure result
+
+    f :: Rope -> (JsonKey, JsonValue) -> Rope
+    f acc (k, v) =
+        acc <> "\n  "
+            <> (intoEscapes pureGrey)
+            <> render 80 k
+            <> (intoEscapes pureGrey)
+            <> " = "
+            <> render 80 v
--- core-telemetry/lib/Core/Telemetry/Honeycomb.hs
+++ core-telemetry/lib/Core/Telemetry/Honeycomb.hs
@@ -1,14 +1,13 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
 
 {- |
-Implementations of different backends that telemetry can be exported to.
+A backend exporter that sends telemetry to Honeycomb.
 -}
-module Core.Telemetry.Backends (
+module Core.Telemetry.Honeycomb (
     Dataset,
-    Exporter,
-    consoleExporter,
     honeycombExporter,
 ) where
 
@@ -24,44 +23,6 @@ import Core.Text.Utilities
 import qualified Data.ByteString as B (ByteString)
 import qualified Data.ByteString.Lazy as L (ByteString)
 import qualified Data.List as List
-
--- TODO convert this into a Render instance
-
--- Somewhat counterintuitively, this does NOT do I/O, but instead returns text
--- which `processTelemetryMessages` will then forward to the main output queue
--- consumed by `processStandardOutput`. This is a bit roundabout, but ensures
--- debug output from this function doesn't smash the console.
-consoleExporter :: Exporter
-consoleExporter =
-    Exporter
-        { codenameFrom = "console"
-        , processorFrom = process
-        }
-  where
-    process :: Datum -> IO Rope
-    process datum = do
-        now <- getCurrentTimeNanoseconds
-        let start = spanTimeFrom datum
-        let text =
-                (intoEscapes pureGrey)
-                    <> spanNameFrom datum
-                    <> " metrics:"
-                    <> let pairs :: [(JsonKey, JsonValue)]
-                           pairs = fromMap (attachedMetadataFrom datum)
-                        in List.foldl' f emptyRope pairs
-                            <> (intoEscapes resetColour)
-
-        let result = formatLogMessage start now SeverityDebug text
-        pure result
-
-    f :: Rope -> (JsonKey, JsonValue) -> Rope
-    f acc (k, v) =
-        acc <> "\n  "
-            <> (intoEscapes pureGrey)
-            <> render 80 k
-            <> (intoEscapes pureGrey)
-            <> " = "
-            <> render 80 v
 
 {- |
 Indicate which \"dataset\" spans and events will be posted into
