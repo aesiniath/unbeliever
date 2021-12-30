70507c37232848aaac66426d721abd263afbdcff
Andrew Cowie <istathar@gmail.com>
2021-12-30 14:34:51 +1100

    Begin writing adapter function to launch a webserver

--- core-webserver-warp/lib/Core/Webserver/Warp.hs
+++ core-webserver-warp/lib/Core/Webserver/Warp.hs
@@ -1,6 +1,25 @@
 module Core.Webserver.Warp where
 
 import Core.Program.Execute (Program)
+import Core.System.Base
 
-launchWebserver :: Program t ()
-launchWebserver = undefined
+import Network.Wai
+import qualified Network.Wai.Handler.Warp as Warp
+
+type Port = Int
+
+launchWebserver :: Port -> Application -> Program τ ()
+launchWebserver port application =
+    let settings =
+            Warp.setOnException
+                onExceptionHandler
+                . Warp.setPort port
+                $ Warp.defaultSettings
+     in do
+        liftIO $ do
+            Warp.runSettings settings application
+        pure ()
+
+onExceptionHandler :: Maybe Request -> SomeException -> IO ()
+onExceptionHandler possibleRequest e = do
+    Warp.defaultOnException possibleRequest e
