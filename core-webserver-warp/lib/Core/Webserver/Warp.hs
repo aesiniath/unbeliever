07b6d9fed6210318c1fd04a128ddacfb2b4de3a8
Andrew Cowie <istathar@gmail.com>
2022-01-02 23:59:50 +1100

    Consolidate logging

--- core-webserver-warp/lib/Core/Webserver/Warp.hs
+++ core-webserver-warp/lib/Core/Webserver/Warp.hs
@@ -38,23 +38,26 @@ loggingMiddleware context0 application request sendResponse = do
     subProgram context0 $ do
         beginTrace $ do
             encloseSpan "Handle request" $ do
-                context <- getContext
-                let path = intoRope (rawPathInfo request)
-                    query = intoRope (rawQueryString request)
-                    method = intoRope (requestMethod request)
-                telemetry
-                    [ metric "request.method" method
-                    , metric "request.path" path
-                    , metric "request.query" query
-                    ]
+                context1 <- getContext
                 liftIO $ do
                     application request $ \response -> do
+                        let path = intoRope (rawPathInfo request)
+                            query = intoRope (rawQueryString request)
+                            method = intoRope (requestMethod request)
+                            status = intoRope (show (statusCode (responseStatus response)))
+
+                        -- actually handle the request
                         result <- sendResponse response
-                        subProgram context $ do
-                            let status = intoRope (show (statusCode (responseStatus response)))
+
+                        -- accumulate the details for logging
+                        subProgram context1 $ do
                             telemetry
-                                [ metric "response.status_code" status
+                                [ metric "request.method" method
+                                , metric "request.path" path
+                                , metric "request.query" query
+                                , metric "response.status_code" status
                                 ]
+
                         pure result
 
 {-
