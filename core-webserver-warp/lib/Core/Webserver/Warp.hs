b06c21fd03b2705c48effd21d3fe6d1d0c0c010e
Andrew Cowie <istathar@gmail.com>
2022-01-03 19:03:54 +1100

    Module level documentation

--- core-webserver-warp/lib/Core/Webserver/Warp.hs
+++ core-webserver-warp/lib/Core/Webserver/Warp.hs
@@ -1,6 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Core.Webserver.Warp where
+{- |
+Many programs present their interface in the form of a webservice, be it
+internet-facing, for internal use, or even just as a machine-local daemon. The
+Haskell language has numerous frameworks for building webservices and a number
+of high-quality HTTP server implementations. This module provides support for
+the Web Application Interface from the __wai__ package and the __warp__
+webserver.
+
+Given an 'Network.Wai.Application' type (the definition of your web service) and a
+'Network.Wai.Middleware' (which is just @Application -> Application@), run a the
+"Core.Program"'s 'Core.Program.Execute.Program' monad. Metrics values (aka web
+server logs) will be sent as key/value pairs via "Core.Telemetry".
+-}
+module Core.Webserver.Warp (
+    Port,
+    launchWebserver,
+) where
 
 --
 -- We follow the convention used elsewhere in this collection of libraries of
@@ -20,6 +36,10 @@ import qualified Network.Wai.Handler.Warp as Warp
 
 type Port = Int
 
+{- |
+Given a WAI 'Application', run a Warp webserver on the specified port from
+within the 'Program' monad.
+-}
 launchWebserver :: Port -> Application -> Program τ ()
 launchWebserver port application =
     let settings =
