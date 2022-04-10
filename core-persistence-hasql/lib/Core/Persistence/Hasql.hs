5206cbab08e48d6c3e63967c7bb1e22e6a6f52e2
Andrew Cowie <istathar@gmail.com>
2022-04-10 22:44:47 +1000

    Introduce new library core-persistence-hasql

---
+++ core-persistence-hasql/lib/Core/Persistence/Hasql.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+{- |
+Reading and writing data to and from a Postgres database.
+
+This module simply contains conveniences for using the most excellent
+__hasql__ library in concert with the rest of the packages in
+this collection.
+-}
+
+module Core.Persistence.Hasql (
+) where
