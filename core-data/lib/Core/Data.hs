396152a4f7b8f1373a3bf638849e724fca1838c0
Andrew Cowie <istathar@gmail.com>
2022-06-05 23:06:19 +1000

    Begin importing TimeStamp type
    Copy TimeStamp implementation from **chronologique** package to new
    module Core.Data.Clock.
    
    Stub out converstion typeclass Instant.

--- core-data/lib/Core/Data.hs
+++ core-data/lib/Core/Data.hs
@@ -12,14 +12,16 @@ import "Core.Data"
 
 as this module re-exports all of its various components.
 -}
-module Core.Data
-  ( -- * Wrappers
+module Core.Data (
+    -- * Wrappers
 
     -- |
     -- Exposes 'Map', a wrapper around a dictionary type, and 'Set', for
     -- collections of elements.
     module Core.Data.Structures,
-  )
-where
+    -- |
+    -- Facilities for making timestamps and for converting between different representations of instants of time.
+    module Core.Data.Clock,
+) where
 
 import Core.Data.Structures
---
+++ core-data/lib/Core/Data/Clock.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralisedNewtypeDeriving #-}
+
+module Core.Data.Clock (
+    -- * Time type
+    TimeStamp,
+
+    -- * Conversions
+    Instant (fromTime, intoTime),
+    unTimeStamp,
+) where
+
+import Core.Text.Bytes (Bytes)
+import Core.Text.Rope (Rope)
+import Data.Bifoldable (Bifoldable)
+import Data.ByteString qualified as B (ByteString)
+import Data.Int (Int64)
+import GHC.Generics
+
+{- |
+Number of nanoseconds since the Unix epoch.
+
+The Show instance displays the TimeStamp as seconds with the nanosecond
+precision expressed as a decimal amount after the interger, ie:
+
+>>> t <- getCurrentTimeNanoseconds
+>>> show t
+2014-07-31T23:09:35.274387031Z
+
+However this doesn't change the fact the underlying representation counts
+nanoseconds since epoch:
+
+>>> show $ unTimeStamp t
+1406848175274387031
+
+There is a Read instance that is reasonably accommodating.
+
+>>> read "2014-07-31T13:05:04.942089001Z" :: TimeStamp
+2014-07-31T13:05:04.942089001Z
+
+>>> read "1406811904.942089001" :: TimeStamp
+2014-07-31T13:05:04.942089001Z
+
+>>> read "1406811904" :: TimeStamp
+2014-07-31T13:05:04.000000000Z
+
+In case you're wondering, the valid range of nanoseconds that fits into the
+underlying Int64 is:
+
+>>> show $ minBound :: TimeStamp
+1677-09-21T00:12:43.145224192Z
+
+>>> show $ maxBound :: TimeStamp
+2262-04-11T23:47:16.854775807Z
+
+so in a quarter millenium's time, yes, you'll have the Y2262 Problem.
+Haskell code from today will, of course, still be running, so in the mid
+Twenty-Third century you will need to replace this implementation with
+something else.
+-}
+newtype TimeStamp = TimeStamp
+    { unTimeStamp :: Int64
+    }
+    deriving (Eq, Ord, Enum, Num, Real, Integral, Bounded, Generic)
+
+class Instant a where
+    fromTime :: TimeStamp -> a
+    intoTime :: a -> TimeStamp
