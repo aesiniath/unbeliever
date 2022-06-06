6c59dddc16cfc895caed895c110c6d920ce53390
Andrew Cowie <istathar@gmail.com>
2022-06-06 21:31:20 +1000

    Qualify direct use of Data.Hourglass types

--- core-data/lib/Core/Data/Clock.hs
+++ core-data/lib/Core/Data/Clock.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE TupleSections #-}
 
 module Core.Data.Clock (
     -- * Time type
@@ -11,21 +12,47 @@ module Core.Data.Clock (
     -- * Conversions
     Instant (fromTime, intoTime),
     unTimeStamp,
+    -- silent
+    convertFromTimeStamp,
 ) where
 
+import Control.Applicative ((<|>))
 import Core.Text.Bytes (Bytes)
-import Core.Text.Rope (Rope)
+import Core.Text.Rope (Rope, Textual (..))
 import Data.Bifoldable (Bifoldable)
 import Data.ByteString qualified as B (ByteString)
+import Data.Hourglass qualified as H (
+    DateTime (..),
+    Elapsed (..),
+    ElapsedP (..),
+    ISO8601_Date (..),
+    ISO8601_DateAndTime (..),
+    NanoSeconds (..),
+    Seconds (..),
+    TimeFormat (..),
+    TimeFormatElem (..),
+    TimeFormatString (..),
+    Timeable (timeGetElapsedP),
+    timeParse,
+    timePrint,
+ )
 import Data.Int (Int64)
+import Data.Maybe (maybeToList)
 import Data.Time.Clock (UTCTime)
-import Data.Time.Clock.POSIX (POSIXTime, posixSecondsToUTCTime, utcTimeToPOSIXSeconds)
+import Data.Time.Clock.POSIX (
+    POSIXTime,
+    posixSecondsToUTCTime,
+    utcTimeToPOSIXSeconds,
+ )
 import GHC.Generics
+import Time.System qualified as H (
+    timeCurrentP,
+ )
 
 {- |
 Number of nanoseconds since the Unix epoch.
 
-The Show instance displays the TimeStamp as seconds with the nanosecond
+The 'Show' instance displays the 'TimeStamp' as seconds with the nanosecond
 precision expressed as a decimal amount after the interger, ie:
 
 >>> t <- getCurrentTimeNanoseconds
@@ -38,7 +65,7 @@ nanoseconds since epoch:
 >>> show $ unTimeStamp t
 1406848175274387031
 
-There is a Read instance that is reasonably accommodating.
+There is a 'Read' instance that is reasonably accommodating:
 
 >>> read "2014-07-31T13:05:04.942089001Z" :: TimeStamp
 2014-07-31T13:05:04.942089001Z
@@ -50,7 +77,7 @@ There is a Read instance that is reasonably accommodating.
 2014-07-31T13:05:04.000000000Z
 
 In case you're wondering, the valid range of nanoseconds that fits into the
-underlying Int64 is:
+underlying 'Int64' is:
 
 >>> show $ minBound :: TimeStamp
 1677-09-21T00:12:43.145224192Z
@@ -69,6 +96,115 @@ newtype TimeStamp = TimeStamp
     { unTimeStamp :: Int64
     }
     deriving (Eq, Ord, Enum, Num, Real, Integral, Bounded, Generic)
+
+instance Show TimeStamp where
+    show t = H.timePrint ISO8601_Precise (convertFromTimeStamp t)
+
+{- |
+Format string describing full (nanosecond) precision ISO8601 time,
+
+> 2014-07-31T23:09:35.274387019Z
+-}
+data ISO8601_Precise = ISO8601_Precise
+
+instance H.TimeFormat ISO8601_Precise where
+    toFormat _ =
+        H.TimeFormatString
+            [ H.Format_Year
+            , H.Format_Text '-'
+            , H.Format_Month2
+            , H.Format_Text '-'
+            , H.Format_Day2
+            , H.Format_Text 'T'
+            , H.Format_Hour
+            , H.Format_Text ':'
+            , H.Format_Minute
+            , H.Format_Text ':'
+            , H.Format_Second
+            , H.Format_Text '.'
+            , H.Format_Precision 9
+            , H.Format_Text 'Z'
+            ]
+
+data ISO8601_Seconds = ISO8601_Seconds
+
+instance H.TimeFormat ISO8601_Seconds where
+    toFormat _ =
+        H.TimeFormatString
+            [ H.Format_Year
+            , H.Format_Text '-'
+            , H.Format_Month2
+            , H.Format_Text '-'
+            , H.Format_Day2
+            , H.Format_Text 'T'
+            , H.Format_Hour
+            , H.Format_Text ':'
+            , H.Format_Minute
+            , H.Format_Text ':'
+            , H.Format_Second
+            , H.Format_Text 'Z'
+            ]
+
+data Posix_Precise = Posix_Precise
+
+instance H.TimeFormat Posix_Precise where
+    toFormat _ =
+        H.TimeFormatString
+            [ H.Format_UnixSecond
+            , H.Format_Text '.'
+            , H.Format_MilliSecond
+            , H.Format_MicroSecond
+            , H.Format_NanoSecond
+            ]
+
+data Posix_Micro = Posix_Micro
+
+instance H.TimeFormat Posix_Micro where
+    toFormat _ =
+        H.TimeFormatString
+            [ H.Format_UnixSecond
+            , H.Format_Text '.'
+            , H.Format_MilliSecond
+            , H.Format_MicroSecond
+            ]
+
+data Posix_Milli = Posix_Milli
+
+instance H.TimeFormat Posix_Milli where
+    toFormat _ =
+        H.TimeFormatString
+            [ H.Format_UnixSecond
+            , H.Format_Text '.'
+            , H.Format_MilliSecond
+            ]
+
+data Posix_Seconds = Posix_Seconds
+
+instance H.TimeFormat Posix_Seconds where
+    toFormat _ =
+        H.TimeFormatString
+            [ H.Format_UnixSecond
+            ]
+
+instance Read TimeStamp where
+    readsPrec _ s = maybeToList $ (,"") <$> parseInput s
+
+parseInput :: String -> Maybe TimeStamp
+parseInput = fmap reduceDateTime . parse
+  where
+    parse :: String -> Maybe H.DateTime
+    parse x =
+        H.timeParse ISO8601_Precise x
+            <|> H.timeParse ISO8601_Seconds x
+            <|> H.timeParse H.ISO8601_DateAndTime x
+            <|> H.timeParse H.ISO8601_Date x
+            <|> H.timeParse Posix_Precise x
+            <|> H.timeParse Posix_Micro x
+            <|> H.timeParse Posix_Milli x
+            <|> H.timeParse Posix_Seconds x
+
+    reduceDateTime :: H.DateTime -> TimeStamp
+    reduceDateTime = convertToTimeStamp . H.timeGetElapsedP
 
 {- |
 Convert between different representations of time.
@@ -107,4 +243,21 @@ say, number of nanoseconds since the Unix epoch).
 @since 0.3.3
 -}
 getCurrentTimeNanoseconds :: IO TimeStamp
-getCurrentTimeNanoseconds = undefined
+getCurrentTimeNanoseconds = do
+    p <- H.timeCurrentP
+    return $! convertToTimeStamp p
+
+--
+-- We're not exposing this as an Instant instance because we want the
+-- dependency on **hourglass** to go away.
+--
+convertToTimeStamp :: H.ElapsedP -> TimeStamp
+convertToTimeStamp (H.ElapsedP (H.Elapsed (H.Seconds seconds)) (H.NanoSeconds nanoseconds)) =
+    let s = fromIntegral seconds :: Int64
+        ns = fromIntegral nanoseconds
+     in TimeStamp $! (s * 1000000000) + ns
+
+convertFromTimeStamp :: TimeStamp -> H.ElapsedP
+convertFromTimeStamp (TimeStamp ticks) =
+    let (s, ns) = divMod ticks 1000000000
+     in H.ElapsedP (H.Elapsed (H.Seconds (s))) (H.NanoSeconds (ns))
