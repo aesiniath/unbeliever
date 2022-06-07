5562759134a35fe7365ec08b9eae461887bbced3
Andrew Cowie <istathar@gmail.com>
2022-06-08 00:05:00 +1000

    Suppress warning
    We're keeping the Core.System.External module but it's presently empty.
    Suppress warnings about that for now.

--- core-program/lib/Core/System.hs
+++ core-program/lib/Core/System.hs
@@ -1,21 +1,24 @@
+{-# OPTIONS_GHC -Wno-dodgy-exports #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
 {-# OPTIONS_HADDOCK not-home #-}
 
--- |
--- Common elements from the rest of the Haskell ecosystem. This is mostly
--- about re-exports. There are numerous types and functions that are more or
--- less assumed to be in scope when you're doing much of anything in Haskell;
--- this module is a convenience to pull in the ones we rely on for the rest of
--- this library.
---
--- You can just import this directly:
---
--- @
--- import "Core.System"
--- @
---
--- as there's no particular benefit to cherry-picking the various sub-modules.
-module Core.System
-  ( -- * Base libraries
+{- |
+Common elements from the rest of the Haskell ecosystem. This is mostly about
+re-exports. There are numerous types and functions that are more or less
+assumed to be in scope when you're doing much of anything in Haskell; this
+module is a convenience to pull in the ones we rely on for the rest of this
+library.
+
+You can just import this directly:
+
+@
+import "Core.System"
+@
+
+as there's no particular benefit to cherry-picking the various sub-modules.
+-}
+module Core.System (
+    -- * Base libraries
 
     -- |
     -- Re-exports from foundational libraries supplied by the compiler runtime,
@@ -39,9 +42,8 @@ module Core.System
     -- pretty printed. There are a large family of combinators used when doing
     -- this. For convenience they are exposed here.
     module Core.System.Pretty,
-  )
-where
+) where
 
 import Core.System.Base
-import Core.System.External ()
+import Core.System.External
 import Core.System.Pretty
