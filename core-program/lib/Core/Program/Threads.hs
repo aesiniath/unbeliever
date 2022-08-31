bf4514949fd75058a593b3be7a7e21dae5729aae
Andrew Cowie <istathar@gmail.com>
2022-09-01 07:36:16 +1000

    Convert threads module to use Ki

--- core-program/lib/Core/Program/Threads.hs
+++ core-program/lib/Core/Program/Threads.hs
@@ -31,7 +31,6 @@ module Core.Program.Threads (
     waitThread_,
     waitThread',
     waitThreads',
-    linkThread,
     cancelThread,
 
     -- * Helper functions
@@ -43,6 +42,7 @@ module Core.Program.Threads (
     -- * Internals
     Thread,
     unThread,
+    getScope,
 ) where
 
 import Control.Concurrent.Async (Async, AsyncCancelled)
@@ -61,6 +61,7 @@ import Control.Concurrent.MVar (
     newMVar,
     readMVar,
  )
+import Control.Concurrent.STM (atomically, orElse)
 import Control.Exception.Safe qualified as Safe (catch, catchAsync, throw)
 import Control.Monad (
     void,
@@ -70,16 +71,24 @@ import Core.Program.Context
 import Core.Program.Logging
 import Core.System.Base
 import Core.Text.Rope
+import Ki qualified as Ki (Scope, Thread, await, awaitAll, fork, scoped)
 
 {- |
 A thread for concurrent computation.
 
 (this wraps __async__'s 'Async')
 -}
-newtype Thread α = Thread (Async α)
+newtype Thread α = Thread (Ki.Thread α)
 
-unThread :: Thread α -> Async α
+unThread :: Thread α -> Ki.Thread α
 unThread (Thread a) = a
+
+getScope :: Context τ -> Ki.Scope
+getScope context =
+    let possibleScope = currentScopeFrom context
+     in case possibleScope of
+            Nothing -> error "Attempt to use a Context that is not within an executing Program monad"
+            Just scope -> scope
 
 {- |
 Fork a thread. The child thread will run in the same @Context@ as the calling
@@ -107,7 +116,7 @@ forkThread program = do
     context <- ask
     let i = startTimeFrom context
     let v = currentDatumFrom context
-
+    let scope = getScope context
     liftIO $ do
         -- if someone calls resetTimer in the thread it should just be that
         -- thread's local duration that is affected, not the parent. We simply
@@ -132,7 +141,7 @@ forkThread program = do
 
         -- fork, and run nested program
 
-        a <- Async.async $ do
+        a <- Ki.fork scope $ do
             Safe.catch
                 (subProgram context' program)
                 ( \(e :: SomeException) ->
@@ -146,7 +155,7 @@ forkThread program = do
 
         return (Thread a)
 
-{-|
+{- |
 Fork a thread with 'forkThread' but do not wait for a result. This is on the
 assumption that the sub program will either be a side-effect and over quickly,
 or long-running daemon thread (presumably containing a 'Control.Monad.forever'
@@ -177,12 +186,8 @@ ensure the behaviour described above)
 -}
 waitThread :: Thread α -> Program τ α
 waitThread (Thread a) = liftIO $ do
-    Safe.catchAsync
-        (Async.wait a)
-        ( \(e :: AsyncCancelled) -> do
-            Async.cancel a
-            Safe.throw e
-        )
+    atomically $ do
+        Ki.await a
 
 {- |
 Wait for the completion of a thread, discarding its result. This is
@@ -209,7 +214,7 @@ value.
 @since 0.2.7
 -}
 waitThread_ :: Thread α -> Program τ ()
-waitThread_ = void . waitThread
+waitThread_ t = void (waitThread t)
 
 {- |
 Wait for a thread to complete, returning the result if the computation was
@@ -226,14 +231,14 @@ being watched as well.
 -}
 waitThread' :: Thread α -> Program τ (Either SomeException α)
 waitThread' (Thread a) = liftIO $ do
-    Safe.catchAsync
+    Safe.catch
         ( do
-            result <- Async.waitCatch a
-            pure result
+            result <- atomically $ do
+                Ki.await a
+            pure (Right result)
         )
-        ( \(e :: AsyncCancelled) -> do
-            Async.cancel a
-            Safe.throw e
+        ( \(e :: SomeException) -> do
+            pure (Left e)
         )
 
 {- |
@@ -278,47 +283,63 @@ described throughout this module)
 @since 0.4.5
 -}
 waitThreads' :: [Thread α] -> Program τ [Either SomeException α]
-waitThreads' ts = liftIO $ do
+waitThreads' ts = liftIO $ undefined
+
+{-
     let as = fmap unThread ts
     Safe.catchAsync
         ( do
-            results <- mapM Async.waitCatch as
+            results <- atomically $ do
+                mapM Ki.await as
             pure results
         )
         ( \(e :: AsyncCancelled) -> do
             mapM_ Async.cancel as
             Safe.throw e
         )
-
-{- |
-Ordinarily if an exception is thrown in a forked thread that exception is
-silently swollowed. If you instead need the exception to propegate back to the
-parent thread, you can \"link\" the two together using this function.
-
-(this wraps __async__\'s 'Control.Concurrent.Async.link')
-
-@since 0.4.2
 -}
-linkThread :: Thread α -> Program τ ()
-linkThread (Thread a) = do
-    liftIO $ do
-        Async.link a
 
 {- |
-Cancel a thread.
+Wait for many threads to complete. This function is intended for the scenario
+where you fire off a number of worker threads with `forkThread` but rather
+than leaving them to run independantly, you need to wait for them all to
+complete.
 
-(this wraps __async__\'s 'Control.Concurrent.Async.cancel'. The underlying
-mechanism used is to throw the 'AsyncCancelled' to the other thread. That
-exception is asynchronous, so will not be trapped by a
-'Core.Program.Exceptions.catch' block and will indeed cause the thread
-receiving the exception to come to an end)
+(this extends __ki__\'s 'Ki.awaitAll' to work across a list of Threads)
 
+@since 0.6.0
+-}
+waitThreads :: [Thread α] -> Program τ ()
+waitThreads ts = liftIO $ do
+    atomically $ do
+        mapM_ (Ki.await . unThread) ts
+
+{- |
+Wait for all child threads in the current scope to complete.
+
+(this wraps __ki__\'s 'Ki.awaitAll' )
+
+@since 0.6.0
+-}
+waitThreadsAll :: Program τ ()
+waitThreadsAll = do
+    context <- ask
+    let possibleScope = currentScopeFrom context
+    scope <- case possibleScope of
+        Nothing -> error "Invalid use of waitThreadsAll without an enclosing scope"
+        Just value -> pure value
+
+    liftIO $ do
+        atomically $ do
+            Ki.awaitAll scope
+
+{- |
 @since 0.4.5
 -}
 cancelThread :: Thread α -> Program τ ()
 cancelThread (Thread a) = do
-    liftIO $ do
-        Async.cancel a
+    error "No longer"
+{-# DEPRECATED cancelThread "No longer implemented" #-}
 
 {- |
 Fork two threads and wait for both to finish. The return value is the pair of
@@ -345,9 +366,18 @@ concurrentThreads :: Program τ α -> Program τ β -> Program τ (α, β)
 concurrentThreads one two = do
     context <- ask
     liftIO $ do
-        Async.concurrently
-            (subProgram context one)
-            (subProgram context two)
+        Ki.scoped $ \scope -> do
+            a1 <- Ki.fork scope $ do
+                subProgram context one
+
+            a2 <- Ki.fork scope $ do
+                subProgram context two
+
+            atomically $ do
+                result1 <- Ki.await a1
+                result2 <- Ki.await a2
+
+                pure (result1, result2)
 
 {- |
 Fork two threads and wait for both to finish.
@@ -361,12 +391,7 @@ running will be cancelled and the original exception is then re-thrown.
 @since 0.4.0
 -}
 concurrentThreads_ :: Program τ α -> Program τ β -> Program τ ()
-concurrentThreads_ one two = do
-    context <- ask
-    liftIO $ do
-        Async.concurrently_
-            (subProgram context one)
-            (subProgram context two)
+concurrentThreads_ one two = void (concurrentThreads one two)
 
 {- |
 Fork two threads and race them against each other. This blocks until one or
@@ -395,9 +420,21 @@ raceThreads :: Program τ α -> Program τ β -> Program τ (Either α β)
 raceThreads one two = do
     context <- ask
     liftIO $ do
-        Async.race
-            (subProgram context one)
-            (subProgram context two)
+        Ki.scoped $ \scope -> do
+            t1 <- Ki.fork scope $ do
+                result1 <- subProgram context{currentScopeFrom = Just scope} one
+                pure (Left result1)
+
+            t2 <- Ki.fork scope $ do
+                result2 <- subProgram context{currentScopeFrom = Just scope} two
+                pure (Right result2)
+
+            result <- atomically $ do
+                orElse
+                    (Ki.await t1)
+                    (Ki.await t2)
+
+            pure result
 
 {- |
 Fork two threads and race them against each other. When one action completes
@@ -418,9 +455,4 @@ timeouts:
 @since 0.4.0
 -}
 raceThreads_ :: Program τ α -> Program τ β -> Program τ ()
-raceThreads_ one two = do
-    context <- ask
-    liftIO $ do
-        Async.race_
-            (subProgram context one)
-            (subProgram context two)
+raceThreads_ one two = void (raceThreads one two)
