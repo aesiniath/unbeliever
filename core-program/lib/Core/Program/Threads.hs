155f7c23492d779bf602f3e8fd778f28a84260ec
Andrew Cowie <istathar@gmail.com>
2023-10-12 18:09:09 +1100

    Document telemetry interaction with forked threads

--- core-program/lib/Core/Program/Threads.hs
+++ core-program/lib/Core/Program/Threads.hs
@@ -126,8 +126,23 @@ Fork a thread. The child thread will run in the same 'Context' as the calling
 'Program', including sharing the user-defined application state value.
 
 If you want to find out what the result of a thread was use 'waitThread' on
-the 'Thread' object returned from this function. If you don't need the
-result, use 'forkThread_' instead.
+the 'Thread' object returned from this function. For example:
+
+@
+    t1 <- 'forkThread' $ do
+        'Core.Program.Logging.info' \"Doing interesting stuff concurrently\"
+        'pure' True
+
+    ...
+
+    result <- 'waitThread' t1
+
+    if result
+        then -- expected
+        else -- not good
+@
+
+If you don't need the result, you can use 'forkThread_' instead.
 
 Threads that are launched off as children are on their own! If the code in the
 child thread throws an exception that is /not/ caught within that thread, the
@@ -135,9 +150,41 @@ exception will kill the thread. Threads dying without telling anyone is a bit
 of an anti-pattern, so this library logs a warning-level log message if this
 happens.
 
-Note that threads inherit the telemetry state of their parent.
+(this function wraps __base__'s 'Control.Concurrent.forkIO')
 
-(this wraps __base__'s 'Control.Concurrent.forkIO')
+/Concerning telemetry/
+
+Note that threads inherit the telemetry state from their parent. If you are
+using the tracing features from __core-telemetry__ any telemetry
+registered in that side task will be included in the enclosing span active in the thread
+that spawned the thread:
+
+@
+    t2 <- 'forkThread' $ do
+        'Core.Program.Logging.info' \"Performing quick side task\"
+        'Core.Telemetry.Observability.telemetry'
+            [ ''Core.Telemetry.Observability.metric' \"counter\" 42
+            ]
+        ...
+
+@
+
+In this case the @\"counter\"@ field in the parent span will get the value
+@42@. This is appropriate for the common case where you are doing small side
+tasks concurrently to accelerate a larger computation.
+
+But at other times you are launching off a fully independent control flow and
+want it to have its own telemetry. In those cases, you'll want to start a new
+span (or even a new trace) immediately after forking the thread:
+
+@
+    'forkThread_' $ do
+        'Core.Telemetry.Observability.encloseSpan' \"subTask\" $ do
+            ...
+@
+
+any telemetry from this worker thread will be appropriately nested in a new
+child span called @\"subTask\"@.
 
 @since 0.2.7
 -}
